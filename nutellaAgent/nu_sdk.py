8552820bfa4eafec8218bc5172fb118b1f1a790e
오송미 <songmi.ohh@gmail.com>
2020-11-15 00:43:24 +0900

    init, log api 연결 완료

--- nutellaAgent/nu_sdk.py
+++ nutellaAgent/nu_sdk.py
@@ -12,31 +12,38 @@ class Nutella(threading.Thread):
         self.config_info = dict() # run의 configuration 정보
         self.system_info = dict() # run의 system 정보
         self.metrics_info = dict() # run의 지표 정보 (시각화를 위한)
-        self.psValue = psutil.Process() # ?
+        self.psValue = psutil.Process()
         self.request_url = "http://localhost:7000/admin/sdk"
 
 
     def init(self, run_name = None, project_key = None, reinit = False):
-        project_id, run_id = asyncio.run(Requests().get_action(parameter1 = project_key, parameter2 = run_name, url = self.request_url))
-        if not project_id:
-            print("Error! The project doesn't exist")
-        else:
-            self.basic_info["run_name"] = run_name
-            self.basic_info["project_key"] = project_key
-            self.basic_info["project_id"] = project_id[0]["projectId"]
-            # run_name으로 새로운 run 만드는 api를 위한 거 추가                
-    
-    # def config(self, **config_datas):
-    #     for key, value in config_datas.items():
-    #         self.config_info[key] = value
-    #     asyncio.run(Requests().post_action(request_datas = self.config_info, url = self.request_url))
+        results = asyncio.run(Requests().get_action(parameter1 = project_key, parameter2 = run_name, url = self.request_url))
+
+        run_id = results["runId"]
+        project_id = results["projectId"]
+
+        self.basic_info["run_id"] = run_id
+        self.basic_info["run_name"] = run_name
+        self.basic_info["project_key"] = project_key
+        self.basic_info["project_id"] = project_id            
 
     def log(self, **metrics_datas):
-        self.metrics_info["run_name"] = self.basic_info["run_name"]
+        self.metrics_info["run_id"] = self.basic_info["run_id"]
+        self.metrics_info["metrics"] = dict()
 
         for key, value in metrics_datas.items():
-            self.metrics_info[key] = value
-        asyncio.run(Requests().post_action(request_datas = self.metrics_info, url = self.request_url))
+            if str(type(value)) == "<class 'float'>":
+                for key, value in metrics_datas.items():
+                    self.metrics_info["metrics"][str(key)] = value                  
+                asyncio.run(Requests().post_action(request_datas = self.metrics_info, url = self.request_url))
+
+                break;
+            else:
+                for i in range(len(value)):
+                    for key, value in metrics_datas.items():
+                        self.metrics_info["metrics"][str(key)] = value[i]
+                    asyncio.run(Requests().post_action(request_datas = self.metrics_info, url = self.request_url))
+                break;
 
     def hardware_system_value(self):
         p = psutil.Process()
